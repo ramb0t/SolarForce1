a6252e74d5dd92df569c9d70bdcfa1e185a4b65d
Rambo <rambo@rambo.co.za>
2015-02-27 00:21:05 +0200

    i2c LCD working!

--- CAN Embedded/Test_Node/TWI_LCD/lcdpcf8574/lcdpcf8574.h
+++ CAN Embedded/Test_Node/TWI_LCD/lcdpcf8574/lcdpcf8574.h
@@ -20,7 +20,7 @@ References:
 
 #define LCD_PCF8574_INIT 1 //init pcf8574
 
-#define LCD_PCF8574_DEVICEID 0 //device id, addr = pcf8574 base addr + LCD_PCF8574_DEVICEID
+#define LCD_PCF8574_DEVICEID 7 //device id, addr = pcf8574 base addr + LCD_PCF8574_DEVICEID
 
 
 /** 
@@ -38,10 +38,10 @@ References:
 
 
 
-#define LCD_DATA0_PIN    7            /**< pin for 4bit data bit 0  */
-#define LCD_DATA1_PIN    6            /**< pin for 4bit data bit 1  */
-#define LCD_DATA2_PIN    5            /**< pin for 4bit data bit 2  */
-#define LCD_DATA3_PIN    4            /**< pin for 4bit data bit 3  */
+#define LCD_DATA0_PIN    4            /**< pin for 4bit data bit 0  */
+#define LCD_DATA1_PIN    5            /**< pin for 4bit data bit 1  */
+#define LCD_DATA2_PIN    6            /**< pin for 4bit data bit 2  */
+#define LCD_DATA3_PIN    7            /**< pin for 4bit data bit 3  */
 #define LCD_RS_PIN       0            /**< pin  for RS line         */
 #define LCD_RW_PIN       1            /**< pin  for RW line         */
 #define LCD_E_PIN        2            /**< pin  for Enable line     */
