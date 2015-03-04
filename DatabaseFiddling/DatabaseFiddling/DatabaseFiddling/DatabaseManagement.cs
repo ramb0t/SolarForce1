550e9e2e47aee83906032b394e05960a97c29d23
Sulaiman S Patel <sulaimansaleempatel@yahoo.co.uk>
2015-03-04 18:51:55 +0200

    DatabaseFiddling now has some fiddling.
    creation of XML file via console successful, checks not overwriting existing file.
    
    Next up: writing entries to XML file.

---
+++ DatabaseFiddling/DatabaseFiddling/DatabaseFiddling/DatabaseManagement.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Xml;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatabaseFiddling
+{
+    class DatabaseManagement
+    {
+        static void Main(string[] args)
+        {
+            Database db = new Database();
+            db.createXML("xmlTrialDb.xml");
+            Console.ReadKey();
+        }
+    }
+
+    class Database
+    {
+        public Database() { }
+
+        public bool createXML(String filename)
+        {
+            bool exists = File.Exists(filename);
+            if (!exists)
+            {
+                XmlTextWriter writer = new XmlTextWriter(filename, Encoding.UTF8);
+                writer.WriteStartDocument();
+                writer.WriteStartElement("Telemetry_Data");
+                writer.WriteEndElement();
+                writer.Close();
+                Console.WriteLine("XML Database Created");
+            }
+            else Console.WriteLine("XML Database already exists.");
+            return exists;
+        }
+
+    }
+}
