60f48ef0332b2122c7ffc3c0f9d4f4661df09e96
Sulaiman S Patel <sulaimansaleempatel@yahoo.co.uk>
2015-03-05 15:24:06 +0200

    Modified to use LINQ
    Node addition code is executing; but not writing to the XML file for some reason.

--- DatabaseFiddling/DatabaseFiddling/DatabaseFiddling/DatabaseManagement.cs
+++ DatabaseFiddling/DatabaseFiddling/DatabaseFiddling/DatabaseManagement.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Xml;
+using System.Xml.Linq;
 using System.IO;
 using System.Text;
 using System.Collections.Generic;
@@ -12,31 +13,65 @@ namespace DatabaseFiddling
     {
         static void Main(string[] args)
         {
-            Database db = new Database();
-            db.createXML("xmlTrialDb.xml");
+            Database db = new Database("xmlTrialDb.xml");
+            db.addData("Speed", 100);
+            db.addData("InputCurrent", 23);
             Console.ReadKey();
         }
     }
 
+    /*
+     * Database Class should manage all interactions with database
+     * including the creation of the database,
+     * adding entries to the database,
+     * extracting information from the database
+    */
     class Database
     {
         public Database() { }
 
-        public bool createXML(String filename)
+        private XElement xml_file;
+        private string file_name;
+        private string root = "DataLog";
+
+        //Constructors
+        public Database(String filename)
         {
-            bool exists = File.Exists(filename);
-            if (!exists)
-            {
-                XmlTextWriter writer = new XmlTextWriter(filename, Encoding.UTF8);
-                writer.WriteStartDocument();
-                writer.WriteStartElement("Telemetry_Data");
-                writer.WriteEndElement();
-                writer.Close();
-                Console.WriteLine("XML Database Created");
-            }
-            else Console.WriteLine("XML Database already exists.");
-            return exists;
+            createXML(filename);
         }
 
+        //Create XML file, return whether it currently exists or not
+        public bool createXML(String filename)
+        {
+            bool exists = File.Exists(filename); //check if specified filename exists
+            if (!exists) //if file does not exist, create file
+            {
+                XDocument x = new XDocument(new XDeclaration("1.0", "utf-", "true"), new XElement(root)); //declare XML version and encoding
+                x.Save(filename); //save XML file
+                Console.WriteLine("XML Database Created"); //confirm message displayed in console
+            }
+            else //XML file already exists
+            {
+                Console.WriteLine("XML Database already exists."); //indicate file already exists in console
+            }
+            xml_file = XElement.Load(filename); //load XML file
+            return exists; //return flag indicating if XML file exists or not
+        }
+
+        public void addData(String label, Object value)
+        {
+            Console.WriteLine("Trying to create element...");
+            try
+            {
+                XElement newX = new XElement(root, new XElement(label, value), new XElement("Time", DateTime.Now));
+                xml_file.Add(newX);
+                Console.WriteLine("Node Addition Code Executed...\n"+newX);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex + "\nNode not created.");
+            }
+        }
     }
 }
