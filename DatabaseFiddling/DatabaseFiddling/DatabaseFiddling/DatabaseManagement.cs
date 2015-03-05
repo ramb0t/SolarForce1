e6f61497558a8d7ce0a80f6044dd889000fbe5d1
Sulaiman S Patel <sulaimansaleempatel@yahoo.co.uk>
2015-03-05 20:33:10 +0200

    cleaned up code a little and added comments.
    next up: performing basic queries >> and how to make them flexible!

--- DatabaseFiddling/DatabaseFiddling/DatabaseFiddling/DatabaseManagement.cs
+++ DatabaseFiddling/DatabaseFiddling/DatabaseFiddling/DatabaseManagement.cs
@@ -9,13 +9,13 @@ using System.Threading.Tasks;
 
 namespace DatabaseFiddling
 {
-    class DatabaseManagement
+    class Test
     {
         static void Main(string[] args)
         {
             Database db = new Database("xmlTrialDb.xml");
-            db.addData("Speed", 100);
-            db.addData("InputCurrent", 23);
+            db.addData("OutputCurrent", 7);
+            db.addData("InclineX", 25);
             Console.ReadKey();
         }
     }
@@ -30,14 +30,13 @@ namespace DatabaseFiddling
     {
         public Database() { }
 
-        private XElement xml_file;
-        private string file_name;
-        private string root = "DataLog";
+        private XElement xml_file; //XElement object to be used for creating nodes
+        private string file_name; //address of XML database
 
         //Constructors
         public Database(String filename)
         {
-            createXML(filename);
+            createXML(filename); 
         }
 
         //Create XML file, return whether it currently exists or not
@@ -46,7 +45,7 @@ namespace DatabaseFiddling
             bool exists = File.Exists(filename); //check if specified filename exists
             if (!exists) //if file does not exist, create file
             {
-                XDocument x = new XDocument(new XDeclaration("1.0", "utf-", "true"), new XElement(root)); //declare XML version and encoding
+                XDocument x = new XDocument(new XDeclaration("1.0", "utf-", "true"), new XElement("DataLog")); //declare XML version and encoding
                 x.Save(filename); //save XML file
                 Console.WriteLine("XML Database Created"); //confirm message displayed in console
             }
@@ -55,24 +54,22 @@ namespace DatabaseFiddling
                 Console.WriteLine("XML Database already exists."); //indicate file already exists in console
             }
             xml_file = XElement.Load(filename); //load XML file
-            file_name = filename;
+            file_name = filename; //set address of XML database
             return exists; //return flag indicating if XML file exists or not
         }
 
+        //add node to XML file
         public void addData(String label, Object value)
         {
             Console.WriteLine("Trying to create element...");
             try
             {
-                XElement newX = new XElement("Packet", new XElement(label, value), new XElement("Time", DateTime.Now));
-                xml_file.Add(newX);
-                Console.WriteLine("Node Addition Code Executed...\n"+newX);
-                xml_file.Save(file_name, SaveOptions.OmitDuplicateNamespaces);
-
+                xml_file.Add(new XElement("Packet", new XElement(label, value), new XElement("Time", DateTime.Now))); //add node
+                xml_file.Save(file_name, SaveOptions.OmitDuplicateNamespaces); //save changes
             }
-            catch (Exception ex)
+            catch (Exception ex) //adding new node may return exception if structure of XML file is breached
             {
-                Console.WriteLine("Error: " + ex + "\nNode not created.");
+                Console.WriteLine("Error: " + ex + "\n\nNode not created."); //display error message
             }
         }
     }
