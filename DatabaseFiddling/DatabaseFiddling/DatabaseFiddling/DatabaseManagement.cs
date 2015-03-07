b52fd05b4a8b5873eda57f9c04131bf3ec5bf01f
Sulaiman S Patel <sulaimansaleempatel@yahoo.co.uk>
2015-03-07 11:11:51 +0200

    Now have node attributes.

--- DatabaseFiddling/DatabaseFiddling/DatabaseFiddling/DatabaseManagement.cs
+++ DatabaseFiddling/DatabaseFiddling/DatabaseFiddling/DatabaseManagement.cs
@@ -14,10 +14,12 @@ namespace DatabaseFiddling
         static void Main(string[] args)
         {
             Database db = new Database("xmlTrialDb.xml");
-            db.setNodeLabel("ErrorPacket");
-            db.addData("OutputCurrent", 7);
-            db.setNodeLabel("Data Packet");
-            db.addData("InclineX", 25);
+            db.setNodeLabel("Packet");
+            db.addData("OutputCurrent", 12, "Type", "Good");
+            db.setNodeLabel("Packet");
+            db.addData("BMS", "ADC exploded... thanks mech!", "Type", "Error");
+            db.setNodeLabel("Packet");
+            db.addData("InclineX", 16.2);
             Console.ReadKey();
         }
     }
@@ -61,13 +63,41 @@ namespace DatabaseFiddling
             return exists; //return flag indicating if XML file exists or not
         }
 
-        //add node to XML file
+        //creates data node for XML file
         public void addData(String label, Object value)
+        {
+            try
+            {
+                XElement x = new XElement(node_label, new XElement(label, value), new XElement("Time", DateTime.Now)); //create node
+                addNode(x);
+            }
+            catch (Exception ex) //adding new node may return exception if structure of XML file is breached
+            {
+                Console.WriteLine("Error: " + ex + "\n\nNode not created."); //display error message
+            }
+        }
+
+        //creates data node for XML file with attributes
+        public void addData(String label, Object value, String attr, Object attrVal)
+        {
+            try
+            {
+                XElement x = new XElement(node_label, new XAttribute(attr, attrVal), new XElement(label, value), new XElement("Time", DateTime.Now)); //create node
+                addNode(x);
+            }
+            catch (Exception ex) //adding new node may return exception if structure of XML file is breached
+            {
+                Console.WriteLine("Error: " + ex + "\n\nNode not created."); //display error message
+            }
+        }
+
+        //adds node to XML file
+        private void addNode(XElement element)
         {
             Console.WriteLine("Trying to create element...");
             try
             {
-                xml_file.Add(new XElement(node_label, new XElement(label, value), new XElement("Time", DateTime.Now))); //add node
+                xml_file.Add(element); //add node
                 xml_file.Save(file_name, SaveOptions.OmitDuplicateNamespaces); //save changes
             }
             catch (Exception ex) //adding new node may return exception if structure of XML file is breached
@@ -79,7 +109,6 @@ namespace DatabaseFiddling
         //get the root node label
         public string getNodeLabel()
         { return node_label; }
-
         //set the root node label
         public void setNodeLabel(string label)
         {
@@ -95,5 +124,7 @@ namespace DatabaseFiddling
             }
             node_label = acceptable ? label : "Level_1"; //set node_label to "Level_1" by default 
         }
+
+        
     }
 }
