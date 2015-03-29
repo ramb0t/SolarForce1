29e61a698ad0cf2c64fb62c721af89b14aab7c3a
Sulaiman S Patel <sulaimansaleempatel@yahoo.co.uk>
2015-03-29 22:39:03 +0200

    This is now beginning to look like a proper software project :)

---
+++ DatabaseFiddling/iKlwa Telemetry System/iKlwa Telemetry System/COM_Port_Select.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace iKlwa_Telemetry_System
+{
+    public partial class COM_Port_Select : Form
+    {
+        private object selected_port = "None";
+
+        public COM_Port_Select()
+        {
+            InitializeComponent();
+        }
+
+        public COM_Port_Select(ref object choice)
+        {
+            InitializeComponent();
+            choice = selected_port;
+        }
+
+        private void COM_Port_Select_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.AddRange(
+                System.IO.Ports.SerialPort.GetPortNames());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            selected_port = comboBox1.SelectedText;
+            this.Dispose();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.AddRange(
+                System.IO.Ports.SerialPort.GetPortNames());
+        }
+    }
+}
--- DatabaseFiddling/iKlwa Telemetry System/iKlwa Telemetry System/Program.cs
+++ DatabaseFiddling/iKlwa Telemetry System/iKlwa Telemetry System/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.IO.Ports;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace iKlwa_Telemetry_System
@@ -25,7 +26,8 @@ namespace iKlwa_Telemetry_System
     {
         private SerialPort port;
 
-        public int baudrate
+        #region accessors for port setup
+        public int baud_rate
         {
             get
             {
@@ -53,19 +55,193 @@ namespace iKlwa_Telemetry_System
                         break;
 
                     case (int)Parities.NoParity:
-                    default:
                         port.Parity = Parity.None;
                         break;
+                    default:
+                        port.Parity = Parity.None;
+                        throw new ArgumentException("Only Odd, Even or No Parity Supported");
                 }
             }
         }
 
+        public string name
+        {
+            get
+            { return port.PortName; }
+            set
+            {
+                if (SerialPort.GetPortNames().Contains(value))
+                    port.PortName = value;
+                else
+                {
+                    port.PortName = "COM9";
+                    throw new ArgumentException("The port " + value + " cannot be found");
+                }
+            }
+        }
 
+        public int data_bits
+        {
+            set
+            {
+                if (value < 1 || value > 9)
+                {
+                    port.DataBits = 8;
+                    throw new ArgumentException("Minimum of 1 and Maximum of 9 stop bits permitted");
+                }
+                else
+                    port.DataBits = value;
+            }
+        }
 
+        public int stop_bits
+        {
+            set
+            {
+                if (value == 1)
+                    port.StopBits = StopBits.One;
+                else if (value == 2)
+                    port.StopBits = StopBits.Two;
+                else
+                {
+                    port.StopBits = StopBits.One;
+                    throw new ArgumentException("Only one or two stop bits supported")
+                }
+            }
+        }
+
+        public int interrupt_bytes_threshold
+        {
+            get { return port.ReceivedBytesThreshold; }
+            set
+            {
+                if (value < 1)
+                {
+                    port.ReceivedBytesThreshold = 1;
+                    throw new ArgumentException("Threshold should be at least 1 byte");
+                }
+                else port.ReceivedBytesThreshold = value;
+            }
+        }
+
+        public int read_timeout
+        {
+            set
+            {
+                if (value < 1)
+                {
+                    port.ReadTimeout = 100;
+                    throw new ArgumentException("There should be finite read timeout.");
+                }
+                else port.ReadTimeout = value;
+            }
+        }
+        #endregion
+
+        #region basic reading and writing functions
+        public int readByte()
+        {
+            int val = port.ReadByte();
+            return val;
+        }
+
+        public byte[] readBytes()
+        {
+            int values = port.BytesToRead;
+            byte[] buffer = new byte[values];
+            port.Read(buffer, 0, values);
+            return buffer;
+        }
+
+        public string readText()
+        {
+            int values = port.BytesToRead;
+            char[] buffer = new char[values];
+            port.Read(buffer, 0, values);
+            return new String(buffer);
+        }
+
+        public void writeBytes(byte[] values)
+        {
+            port.Write(values, 0, values.Length);
+        }
+
+        public void writeByte(byte val)
+        {
+            string data = val.ToString();
+            port.Write(data);
+        }
+
+        public void writeText(string msg)
+        {
+            port.Write(msg);
+        }
+
+        #endregion
+
+        private SerialDataReceivedEventHandler current_DREH;
+        protected delegate void general_data_received_handler(object o, SerialDataReceivedEventArgs e);
+        public void set_data_received_event_handler(general_data_received_handler data_received_handler)
+        {
+            if (!current_DREH.Equals(null))
+                port.DataReceived -= current_DREH;
+            current_DREH = new SerialDataReceivedEventHandler(data_received_handler);
+            port.DataReceived += current_DREH;
+        }
 
         public CommsManager()
         {
-            
+            port = new SerialPort();
+            defaultInit();
+        }
+
+        protected void defaultInit()
+        {
+            try
+            {
+                baud_rate = 4800;
+                name = "COM9";
+                data_bits = 8;
+                stop_bits = 2;
+                parity = (int)Parities.Odd;
+                interrupt_bytes_threshold = 1;
+            }
+            catch (Exception e)
+            { MessageBox.Show(e.Message); }
+        }
+
+        public void OpenPort()
+        {
+            if (port.IsOpen)
+                port.Close();
+            try
+            {
+                port.Open();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        protected void Clear()
+        {
+            port.DiscardInBuffer();
+        }
+    }
+
+    class iKlwaComms:CommsManager
+    {
+        private byte pingReq = 0xAA;
+        private byte ACK = 0x55;
+
+        public bool checkForPing()
+        {
+            int readVal = this.readByte();
+            bool pingFound = (readVal==pingReq);
+            if (pingFound)
+                this.writeByte(ACK);
+            return pingFound;
         }
     }
 }
