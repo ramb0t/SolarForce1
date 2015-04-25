1d1dd17db5553bfe228091d79b80f4e8be4f7460
rambo <rambo@rambo.co.za>
2015-04-25 16:27:37 +0200

    Revised MAVLink library files with new defs

---
+++ CAN Embedded/MAVLink/SolarCarREVISED/solarCar/version.h
@@ -0,0 +1,12 @@
+/** @file
+ *	@brief MAVLink comm protocol built from solarCar.xml
+ *	@see http://mavlink.org
+ */
+#ifndef MAVLINK_VERSION_H
+#define MAVLINK_VERSION_H
+
+#define MAVLINK_BUILD_DATE "Sat Apr 25 16:25:18 2015"
+#define MAVLINK_WIRE_PROTOCOL_VERSION "1.0"
+#define MAVLINK_MAX_DIALECT_PAYLOAD_SIZE 60
+ 
+#endif // MAVLINK_VERSION_H
