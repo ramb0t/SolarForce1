da2e625d7b8b8f6717f52ea6ab31010a33e56e91
MattDN93 <matthew.deneef@gmail.com>
2015-04-25 15:51:13 +0200

    Final tweaks to DB and msg defs for Tx

--- CAN Embedded/MAVLink/AVR_MAVLink_Messaging/MAVLink_Messaging/MAVLink_Messaging.h
+++ CAN Embedded/MAVLink/AVR_MAVLink_Messaging/MAVLink_Messaging/MAVLink_Messaging.h
@@ -43,45 +43,57 @@ typedef struct
 	//------------BMS DATA----------------//
 	//BMS Data Flags
 	uint8_t		BMSData_warnings[7];		//Warnings and flags
-	//BMS voltages & IDs
-	uint16_t	packVoltage;				//Voltages
-	uint8_t		maxVoltage;	
-	uint8_t		maxVoltageID;
-	uint8_t		minVoltage;
-	uint8_t		minVoltageID;
-	
+	//BMS voltages & IDs			BIT
+	uint8_t		maxVoltage;			//4
+	uint8_t		maxVoltageID;		//5
+	uint8_t		minVoltage;			//2
+	uint8_t		minVoltageID;		//3
+	uint16_t	packVoltage;		//0,1		//Voltages
+		
 	//BMS Charge Limits					
-	uint16_t	chargeLimit;
-	uint16_t	dischargeLimit;
-	uint16_t	current;
-	
+	uint16_t	current;			//0,1
+	uint16_t	chargeLimit;		//2,3
+	uint16_t	dischargeLimit;		//4,5
+		
 	//BMS Battery Energy
-	uint32_t	batteryEnergyIn;
-	uint32_t	batteryEnergyOut;
+	uint32_t	batteryEnergyIn;	//0-3
+	uint32_t	batteryEnergyOut;	//4-7
 	
 	//BMS Battery Params
-	uint16_t	capacity;					//capacity of bank
-	uint16_t	DOD;						//discharge depth
-	uint8_t		SOC;						//state of charge	
+	uint8_t		SOC;				//0	
+	uint16_t	DOD;				//1,2	//discharge depth
+	uint16_t	capacity;			//3,4		//capacity of bank
+	//byte 5 blank						
+	uint8_t		SOH;				//6		//state of charge	
 	
 	//Temperatures
-	uint8_t		maxTemperature;
-	uint8_t		maxTempID;
-	uint8_t		minTemperature;
-	uint8_t		minTempID;
-	uint8_t		temperature;
-	
+	uint8_t		minTempID;			//0
+	//byte 1 blank
+	uint8_t		minTemperature;		//2
+	uint8_t		temperature;		//3
+	uint8_t		maxTemperature;		//4	
+	uint8_t		maxTempID;			//5
+		
 	//Resistances
-	uint16_t	packResistance;
-	uint8_t		minRes;
-	uint8_t		minResID;
-	uint8_t		maxRes;
-	uint8_t		maxResID;
+	uint16_t	packResistance;		//0,1
+	uint8_t		minRes;				//2	
+	uint8_t		minResID;			//3
+	uint8_t		maxRes;				//4
+	uint8_t		maxResID;			//5
 //------------MPPT DATA----------------//	
-	uint16_t		Vin;
-	uint16_t		Vout;
-	uint8_t			Tamb;
-	uint8_t			mppt_flags;
+	uint16_t		Vin;			//0&1
+	uint16_t		Iin;			//2&3
+	uint16_t		Vout;			//4&5
+	uint8_t			Tamb;			//6
+	uint8_t			mppt_flags;		//bits 4-7 of byte 1
+	
+//------------SPEED DATA----------------//
+	uint8_t			avgSpeed;		//0
+	uint8_t			hesSPeed;		//1
+	uint16_t		hesRPM;			//2,3
+	uint8_t			motorSpeed;		//4
+	uint16_t		motorRPM;		//5,6
+	uint8_t			statusFlags;	//7
 	
 }GlobalVars;
 
