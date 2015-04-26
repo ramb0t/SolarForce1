d036ebae54b87c7d4abcc177223365cd16e13a1a
MattDN93 <matthew.deneef@gmail.com>
2015-04-26 11:34:47 +0200

    CAN Rx defs for BMS data on Tx board

--- CAN Embedded/MAVLink/AVR_MAVLink_Messaging/MAVLink_Messaging/CAN_Int0.c
+++ CAN Embedded/MAVLink/AVR_MAVLink_Messaging/MAVLink_Messaging/CAN_Int0.c
@@ -76,11 +76,11 @@ uint8_t CAN_Decode(CANMessage *message)
 				{
 							//LED_DIAG_PORT |= (1<<LED_DIAG_GRN);		
 							CANData.avgSpeed = message->data[1];
-							CANData.hesSPeed = 15/*message->data[1]*/;
-							CANData.hesRPM = (message->data[0]<<8)|(message->data[1]);/*(message->data[3])|(message->data[2]<<8)*/;
-							CANData.motorSpeed = 18/*message->data[4]*/;
-							CANData.motorRPM =12 /*(message->data[6])|(message->data[5]<<8)*/;
-							CANData.statusFlags = 12/*message->data[7]*/;
+							CANData.hesSPeed = message->data[1];
+							CANData.hesRPM = (message->data[3])|(message->data[2]<<8);
+							CANData.motorSpeed = message->data[4];
+							CANData.motorRPM = (message->data[6])|(message->data[5]<<8);
+							CANData.statusFlags = message->data[7];
 						//
 						
 						speedMDUpdated=1;	
@@ -117,10 +117,6 @@ uint8_t CAN_Decode(CANMessage *message)
 					
 				case	BMS_2_CANID:
 				{
-					//uart_puts("\n");
-					//uart_puts("CAN from BMS2:");					//Human readable data on UART
-					//uart_puts(message->id);
-					
 					utoa(message->data[3],buff,10);		//fault flags
 					//BMS_Message.data[0] = message->data[3];
 					
@@ -132,40 +128,55 @@ uint8_t CAN_Decode(CANMessage *message)
 						//BMS_Message.data[i] = message->data[i]; //store into CAN object for BMS
 						//uart_puts(buff);
 					//}
+					CANData.BMSData_warnings[0] = message->data[0];
+					CANData.BMSData_warnings[1] = message->data[3];
+					CANData.BMSData_warnings[2] = message->data[4];
+					CANData.BMSData_warnings[3] = message->data[5];
+					CANData.BMSData_warnings[4] = message->data[6];
+					
 					bms2Updated=1;
 					return CAN_MSG_DECODED;
 				}
 					
 				case	BMS_3_CANID:
 				{
+					CANData.packVoltage = (message->data[0]<<8)|(message->data[1]);
+					CANData.minVoltage = message->data[2];
+					CANData.minVoltageID = message->data[3];
+					CANData.maxVoltage = message->data[4];
+					CANData.maxVoltageID = message->data[5];
+					
 					//uart_puts("\n");
 					//uart_puts("CAN from BMS3:");					//Human readable data on UART
-					uart_puts(message->id);
-					
-					utoa(message->data[0],buff,10);		//low pack voltage
-					//BMS_Message.data[5] = (message->data[2]<<8)|(message->data[3]);
-					//uart_puts(buff);
-					
-					utoa(message->data[0],buff,10);		// avg pack voltages
-					//BMS_Message.data[6] = (message->data[0]<<8)|(message->data[1]);
-					//uart_puts(buff);
-					
-					utoa(message->data[0],buff,10);		//high pack voltages
-					//BMS_Message.data[7] = (message->data[4]<<8)|(message->data[5]);
-					//uart_puts(buff);
-					
+					//uart_puts(message->id);
+					//
+					//utoa(message->data[0],buff,10);		//low pack voltage
+					////BMS_Message.data[5] = (message->data[2]<<8)|(message->data[3]);
+					////uart_puts(buff);
+					//
+					//utoa(message->data[0],buff,10);		// avg pack voltages
+					////BMS_Message.data[6] = (message->data[0]<<8)|(message->data[1]);
+					////uart_puts(buff);
+					//
+					//utoa(message->data[0],buff,10);		//high pack voltages
+					////BMS_Message.data[7] = (message->data[4]<<8)|(message->data[5]);
+					////uart_puts(buff);
+					//
 					bms3Updated=1;
 					return CAN_MSG_DECODED;
 				}
 				
 				case	BMS_4_CANID:
 				{
+					CANData.current = (message->data[0]<<8)|(message->data[1]);
+					CANData.chargeLimit = (message->data[2]<<8)|(message->data[3]);
+					CANData.dischargeLimit = (message->data[4]<<8)|(message->data[5]);
 					//uart_puts("\n");
 					//uart_puts("CAN from BMS4:");					//Human readable data on UART
 					//uart_puts(message->id);
 					
-					utoa(message->data[0],buff,10);		//pack current
-					utoa(message->data[1],buff,10);
+					//utoa(message->data[0],buff,10);		//pack current
+					//utoa(message->data[1],buff,10);
 					//BMS_Message.data[2] = (message->data[0]<<8)|(message->data[1]);
 					//uart_puts(buff);
 					
@@ -173,14 +184,28 @@ uint8_t CAN_Decode(CANMessage *message)
 					return CAN_MSG_DECODED;
 				}
 				
+				case	BMS_5_CANID:
+				{
+					CANData.batteryEnergyIn = (message->data[0]<<24)|(message->data[1]<<16)|(message->data[2]<<8)|(message->data[3]);
+					CANData.batteryEnergyIn = (message->data[4]<<24)|(message->data[5]<<16)|(message->data[6]<<8)|(message->data[7]);
+					
+					bms5Updated=1;
+					return CAN_MSG_DECODED;
+				}
+				
 
 				case	BMS_6_CANID:
 				{
+					CANData.SOC = message->data[0];
+					CANData.DOD = (message->data[1]<<8)|(message->data[2]);
+					CANData.capacity = (message->data[3]<<8)|(message->data[4]);
+					CANData.SOH = message->data[6];
+					
 					//uart_puts("\n");
 					//uart_puts("CAN from BMS6:");					//Human readable data on UART
 					//uart_puts(message->id);
 				
-					utoa(message->data[0],buff,10);		//SOC
+					//utoa(message->data[0],buff,10);		//SOC
 					//BMS_Message.data[3] = message->data[0];
 					//uart_puts(buff);
 					
@@ -193,24 +218,39 @@ uint8_t CAN_Decode(CANMessage *message)
 					//uart_puts("\n");
 					//uart_puts("CAN from BMS7:");					//Human readable data on UART
 					//uart_puts(message->id);
-				
-					utoa(message->data[2],buff,10);			//Min temps
-					//uart_puts(buff);
-					utoa(message->data[3],buff,10);			//Min temps
-					//uart_puts(buff);
-					//BMS_Message.data[8] = (message->data[2]<<8)|(message->data[3]);
+				//
+					//utoa(message->data[2],buff,10);			//Min temps
+					////uart_puts(buff);
+					//utoa(message->data[3],buff,10);			//Min temps
+					////uart_puts(buff);
+					////BMS_Message.data[8] = (message->data[2]<<8)|(message->data[3]);
+					//
+				//
+					//utoa(message->data[0],buff,10);			//Avg temps
+					////BMS_Message.data[9] = message->data[0];
+					////uart_puts(buff);
+				//
+					//utoa(message->data[4],buff,10);			//Max temps
+					////BMS_Message.data[10] = (message->data[4]<<8)|(message->data[5]);
+					////uart_puts(buff);
+					//
 					
-				
-					utoa(message->data[0],buff,10);			//Avg temps
-					//BMS_Message.data[9] = message->data[0];
-					//uart_puts(buff);
-				
-					utoa(message->data[4],buff,10);			//Max temps
-					//BMS_Message.data[10] = (message->data[4]<<8)|(message->data[5]);
-					//uart_puts(buff);
+					CANData.minTempID = message->data[0];
+					CANData.minTemperature = message->data[2];
+					CANData.temperature = message->data[3];
+					CANData.maxTemperature = message->data[4];
+					CANData.maxTempID = message->data[5];
 					
 					bms7Updated=1;
 					return CAN_MSG_DECODED;
+				}
+				
+				case BMS_8_CANID:
+				{
+					CANData.packResistance = (message->data[0]<<8)|(message->data[1]);
+					CANData.minRes = message->data[2];
+					CANData.minResID = message->data[3];
+					CANData.maxRes = message->data[4];
 				}
 				
 				case	ACGY1 || ACGY2 || ACGY3:
@@ -247,12 +287,29 @@ uint8_t CAN_Decode(CANMessage *message)
 					//uart_puts("\n");
 					//uart_puts("CAN from MPPT1:");
 					//uart_puts(message->id);
-					for (int i=0;i<4;i++)						//4 data fields
-					{
-						utoa(message->data[i],buff,10);
-						//MPPT1_Message.data[i] = message->data[i];
-						//uart_puts(buff);
-					}
+					//for (int i=0;i<4;i++)						//4 data fields
+					//{
+						//utoa(message->data[i],buff,10);
+						////MPPT1_Message.data[i] = message->data[i];
+						////uart_puts(buff);
+					//}
+					
+					uint16_t temp = ((message->data[0] & (0x03))<<8);
+					temp &= 0xFF00;
+					temp |= (message->data[1] & (0x00FF));
+					CANData.Vin1 = temp;
+					
+					uint16_t temp2 = ((message->data[2] & (0x03))<<8);
+					temp &= 0xFF00;
+					temp |= (message->data[3] & (0x00FF));
+					CANData.Iin1 = temp2;
+					
+					uint16_t temp3 = ((message->data[4] & (0x03))<<8);
+					temp &= 0xFF00;
+					temp |= (message->data[5] & (0x00FF));
+					CANData.Vin1 = temp3;
+					
+					CANData.Tamb1 = message->data[6];
 					
 					mppt1Updated=1;
 					return CAN_MSG_DECODED;
@@ -260,15 +317,22 @@ uint8_t CAN_Decode(CANMessage *message)
 				
 				case	MPPT2_CANID:
 				{
-				//uart_puts("\n");
-				//uart_puts("CAN from MPPT1:");
-				//uart_puts(message->id);
-				for (int i=0;i<4;i++)						//4 data fields
-				{
-					utoa(message->data[i],buff,10);
-					//MPPT2_Message.data[i] = message->data[i];
-					//uart_puts(buff);
-				}
+					uint16_t temp = ((message->data[0] & (0x03))<<8);
+					temp &= 0xFF00;
+					temp |= (message->data[1] & (0x00FF));
+					CANData.Vin2 = temp;
+					
+					uint16_t temp2 = ((message->data[2] & (0x03))<<8);
+					temp &= 0xFF00;
+					temp |= (message->data[3] & (0x00FF));
+					CANData.Iin2 = temp2;
+					
+					uint16_t temp3 = ((message->data[4] & (0x03))<<8);
+					temp &= 0xFF00;
+					temp |= (message->data[5] & (0x00FF));
+					CANData.Vin2 = temp3;
+					
+					CANData.Tamb2 = message->data[6];
 				
 				mppt2Updated=1;
 				return CAN_MSG_DECODED;
@@ -276,15 +340,22 @@ uint8_t CAN_Decode(CANMessage *message)
 				
 				case	MPPT3_CANID:
 				{
-				//uart_puts("\n");
-				//uart_puts("CAN from MPPT1:");
-				//uart_puts(message->id);
-				for (int i=0;i<4;i++)						//4 data fields
-				{
-					utoa(message->data[i],buff,10);
-					//MPPT3_Message.data[i] = message->data[i];
-					//uart_puts(buff);
-				}
+					uint16_t temp = ((message->data[0] & (0x03))<<8);
+					temp &= 0xFF00;
+					temp |= (message->data[1] & (0x00FF));
+					CANData.Vin3 = temp;
+					
+					uint16_t temp2 = ((message->data[2] & (0x03))<<8);
+					temp &= 0xFF00;
+					temp |= (message->data[3] & (0x00FF));
+					CANData.Iin3 = temp2;
+					
+					uint16_t temp3 = ((message->data[4] & (0x03))<<8);
+					temp &= 0xFF00;
+					temp |= (message->data[5] & (0x00FF));
+					CANData.Vin3 = temp3;
+					
+					CANData.Tamb3 = message->data[6];
 				
 				mppt3Updated=1;
 				return CAN_MSG_DECODED;
@@ -292,15 +363,22 @@ uint8_t CAN_Decode(CANMessage *message)
 				
 				case	MPPT4_CANID:
 				{
-				//uart_puts("\n");
-				//uart_puts("CAN from MPPT1:");
-				//uart_puts(message->id);
-				for (int i=0;i<4;i++)						//4 data fields
-				{
-					utoa(message->data[i],buff,10);
-					//MPPT4_Message.data[i] = message->data[i];
-					//uart_puts(buff);
-				}
+					uint16_t temp = ((message->data[0] & (0x03))<<8);
+					temp &= 0xFF00;
+					temp |= (message->data[1] & (0x00FF));
+					CANData.Vin4 = temp;
+									
+					uint16_t temp2 = ((message->data[2] & (0x03))<<8);
+					temp &= 0xFF00;
+					temp |= (message->data[3] & (0x00FF));
+					CANData.Iin4 = temp2;
+									
+					uint16_t temp3 = ((message->data[4] & (0x03))<<8);
+					temp &= 0xFF00;
+					temp |= (message->data[5] & (0x00FF));
+					CANData.Vin4 = temp3;
+									
+					CANData.Tamb4 = message->data[6];
 				
 				mppt4Updated=1;
 				return CAN_MSG_DECODED;
